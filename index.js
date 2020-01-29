8d74f2a5c01e56ac3325ae243f141026515aacd8
Nadine <ouronm@techagr.com>
2020-01-29 01:35:23 -0300

    Create handleClass function

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const filePath = './input.csv'
 getJsonFromCsv = async filePath => (csv({
   noheader: true,
   headers: [
-    'fullName', 
-    'eId', 
+    'fullname', 
+    'eid', 
     'class1', 
     'class2', 
     'address1', 
@@ -17,39 +17,47 @@ getJsonFromCsv = async filePath => (csv({
     'address5', 
     'address6', 
     'invisible', 
-    'seeAll'
+    'see_all'
   ]
 }).fromFile(filePath).then(json => json))
+
+const parseSlashOnComma = str => str.replace(/\//g, ',')
+
+const handleClass = (student, cl) => {
+  formatedClass = parseSlashOnComma(cl)
+  const arr = formatedClass.split(',')
+  _.map(arr, c => c && student.push(c.trim()))
+  return student.length === 1 ? student[0] : student
+}
 
 const main = async () => {
   const json = await getJsonFromCsv(filePath)
   const data = _.without(json, json[0])
   const headers = json[0]
 
-  const groupedByName = _.groupBy(data, 'fullName')
+  const groupedByName = _.groupBy(data, 'fullname')
   const names = _.map(groupedByName, (name, key) => key)
+  const studentList = []
 
   _.map(names, name => {
     const student = { 
-      fullName: undefined, 
-      eId: undefined,
+      fullname: undefined, 
+      eid: undefined,
       classes: [],
       addresses: { address1: [], address2: [], address3: [], address4: [], address5: [], address6: [] },
       invisible: undefined, 
-      seeAll: undefined
+      see_all: undefined
     }
-    const { fullName, eId, invisible, seeAll } = groupedByName[name][0]
+    const { fullname, eid, invisible, see_all } = groupedByName[name][0]
 
-    student.fullName = fullName || undefined
-    student.eId = eId || undefined
+    student.fullname = fullname || undefined
+    student.eid = eid || undefined
     student.invisible = invisible || undefined
-    student.seeAll = seeAll || undefined
+    student.see_all = see_all || undefined
 
     _.map(groupedByName[name], info => {
       const { class1, class2, address1, address2, address3, address4, address5, address6 } = info
-      if (class1) student.classes.push(class1)
-      if (class2) student.classes.push(class2)
-
+      student.classes = handleClass(student.classes, `${class1},${class2}`)
       const addresses = { address1, address2, address3, address4, address5, address6 }
       _.map(Object.entries(addresses), entry => {
         const key = entry[0]
@@ -57,8 +65,9 @@ const main = async () => {
         if (value) student.addresses[key].push(value)
       })
     })
+    studentList.push(student)
     console.log(student)
   })
+  // console.log(studentList)
 }
-
 main()
